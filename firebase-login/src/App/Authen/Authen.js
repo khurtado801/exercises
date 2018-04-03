2f228e8cba6094db89cabf013526517880f18133
ken.hurtado <khurtado@protonmail.com>
2018-04-03 15:39:57 -0600

    Finished logout from Firebase. Fixed hide logout button until login and email address field clears after logon. Finished logout from Firebase.

--- firebase-login/src/App/Authen/Authen.js
+++ firebase-login/src/App/Authen/Authen.js
@@ -16,8 +16,15 @@ class Authen extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            err: ''
+            inputs: {
+                email: '',
+                password: '',
+            },
+            err: '',
         };
+    }
+    handleChange = (e) => {
+        this.setState({ emailValue: e.target.value });
     }
 
     login = (event) => {
@@ -26,8 +33,19 @@ class Authen extends React.Component {
         console.log(email, password);
 
         const auth = firebase.auth();
-
         const promise = auth.signInWithEmailAndPassword(email, password);
+
+        promise
+        .then(user => {
+            // Get logout element
+            let err = 'Welcome ' + user.email;
+            console.log('Welcome err', err);
+            let lout = document.getElementById('logout');
+            lout.classList.remove('hide')
+            this.setState({ err: err, emailValue: '' });
+        });
+        
+
         promise.catch(e => {
             let err = e.message;
             console.log(err);
@@ -48,29 +66,37 @@ class Authen extends React.Component {
         .then(user => {
             let err = 'Welcome ' + user.email;
             // Make entry for user in database
-            firebase.database().ref('users/'+user.uid).set({
+            firebase.database().ref('users/' + user.uid).set({
                 email: user.email
             });
             console.log(user);
-            this.setState({err: err});
+            this.setState({ err: err });
         });
         promise
         .catch(e => {
             let err = e.message;
             console.log(err);
-            this.setState({err: err});
+            this.setState({ err: err });
         })
+    }
+
+    logout = () => {
+        firebase.auth().signOut();
+        let lout = document.getElementById('logout');
+        let err = 'Goodbye!';
+        this.setState({ err: err })
+        lout.classList.add('hide');
     }
 
     render() {
         return (
             <div>
-                <input id="email" ref="email" type="email" placeholder="Enter your email" /><br />
+                <input id="email" ref="email" type="email" value={this.state.emailValue} onChange={this.handleChange} placeholder="Enter your email" /><br />
                 <input id="pass" ref="password" type="password" placeholder="Enter your password" /><br />
                 <p>{this.state.err}</p>
                 <button onClick={this.login} >Log in</button>
                 <button onClick={this.signup} >Sign Up</button>
-                <button onClick={this.logout} >Log Out</button>
+                <button onClick={this.logout} id="logout" className="hide" onClick={this.logout} >Log Out</button>
             </div>
         );
     }
