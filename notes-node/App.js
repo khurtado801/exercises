58b51014a445189cf2c91501c5854d175eee72e5
ken.hurtado <khurtado@protonmail.com>
2018-04-07 14:28:36 -0600

    Added section on 'require'.

--- notes-node/App.js
+++ notes-node/App.js
@@ -1,1 +1,17 @@
 console.log('Starting app.');
+
+const fs = require('fs');
+const os = require('os');
+const notes = require('./notes.js')
+
+// A third parameter is need for appendFile, in this option we use a call-back as the third parameter
+// fs.appendFile('greetings.txt', 'Hello, World! This is option 1.', function (err) {
+//     if (err) {
+//         console.log('Unable to write to file');
+//     }
+// });
+
+let user = os.userInfo();
+
+// A third parameter is need for appendFile, this time we call appendFileSync
+fs.appendFileSync('greetings.txt', `Hello ${user.username}!` );
---
+++ notes-node/notes.js
@@ -0,0 +1,6 @@
+console.log('Starting mode.js');
+
+module.exports.age = 25;
+
+// ES6 module
+// export const age = 25;
