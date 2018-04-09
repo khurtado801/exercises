17c060638b3fb44b6d73e305fabe45165b9af63e
ken.hurtado <khurtado@protonmail.com>
2018-04-09 13:37:36 -0600

    Add duplicate note title check.

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -9,15 +9,34 @@ let addNote = (title, body) => {
         body: body
     };
 
-    // Read contents of notes-data.json, which is a string, and save to notesString
-    let notesString = fs.readFileSync('notes-data.json');
-    // Parse file we read to convert to object and save in notes
-    notes = JSON.parse(notesString);
+    
+    try {
+        // Read contents of notes-data.json, which is a string, and save to notesString
+        let notesString = fs.readFileSync('notes-data.json');
+        // Parse file we read to convert to object and save in notes
+        notes = JSON.parse(notesString);
 
-    // Add note to notes array at the end using push
-    notes.push(note);
-    // Update the file named notes-data.json with content to be saved json.stringify(notes) 
-    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    // Catch block error argument variable 'e'
+    } catch (e) {
+
+    }
+
+    // Check notes array for duplicate note...
+    let duplicateNotes = notes.filter((note) => {
+        // If a title matches another title, return true and save it to array duplicateNotes
+        return note.title === title;
+    /* 
+        Check notes array for duplicate note using ES6 arrow function
+        let duplicateNotes = notes.filter((note) => note.title === title)
+    */
+    })
+    // If length of duplicateNotes is greater than 0, that means we do not want to save the note. Because a note with that name already exists. If it is 0, save the note.
+    if (duplicateNotes.length === 0) {
+        // Add note to notes array at the end using push
+        notes.push(note);
+        // Update the file named notes-data.json with content to be saved json.stringify(notes) 
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    }
 };
 
 let getAll = () => {
