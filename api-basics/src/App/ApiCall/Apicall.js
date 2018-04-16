407e1031f03a29fbf2db48994c55decb8c78d7a6
ken.hurtado <khurtado@protonmail.com>
2018-04-16 16:43:55 -0600

    First commit.

---
+++ api-basics/src/App/ApiCall/Apicall.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import './Apicall.css';
+
+class Apicall extends Component {
+    render () {
+        return (
+            <div>
+               <div className="Apititle">
+                    I'm from Apicall
+               </div>
+            </div>
+        );
+    }
+}
+
+export default Apicall;
