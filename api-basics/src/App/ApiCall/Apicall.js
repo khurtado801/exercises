3c6366f1f524055ac9dcdfc601422cfca6ffd3ec
ken.hurtado <khurtado@protonmail.com>
2018-04-16 17:10:59 -0600

    Finished. Connect app to Reddit api.

--- api-basics/src/App/ApiCall/Apicall.js
+++ api-basics/src/App/ApiCall/Apicall.js
@@ -4,11 +4,35 @@ import axios from 'axios';
 import './Apicall.css';
 
 class Apicall extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: [],
+            subr: 'space'
+        };
+    }
+
+    componentWillMount() {
+        this.getReddit();
+    }
+
+    getReddit = () => {
+        axios.get(`https://www.reddit.com/r/${this.state.subr}.json`)
+        .then(res => {
+            const posts = res.data.data.children.map(obj => obj.data);
+            this.setState({ posts });
+        });
+    }
+
     render () {
         return (
             <div>
-               <div className="Apititle">
-                    I'm from Apicall
+               <div className="main-text">
+                    <h1>{`/r/${this.state.subr}`}</h1>
+                    <ul>
+                        {this.state.posts.map(post =>
+                            <li key={post.id}>{post.title}</li>)}
+                    </ul>
                </div>
             </div>
         );
