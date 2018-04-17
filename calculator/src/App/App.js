c16f9f5aba2822676865d076aaeb695967cffa26
ken.hurtado <khurtado@protonmail.com>
2018-04-17 17:09:04 -0600

    Completed event listeners for: performOperation, AutoShrinkText. Calculator finished.

--- calculator/src/App/App.js
+++ calculator/src/App/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
 import Calculator from './Calculator/Calculator';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Calculator />
+        <div id="wrapper">
+          <div id="app">
+            <Calculator />
+          </div>
+        </div>
       </div>
     );
   }
