28f7eabc6e2f7c1d9bb1f1b70c3680966a64361c
ken.hurtado <khurtado@protonmail.com>
2018-04-17 11:40:20 -0600

    Added HTML framework for calculator.

--- calculator/src/App/App.js
+++ calculator/src/App/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 
-import './App.css';
+import Calculator from './Calculator/Calculator';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+      <div>
+        <Calculator />
       </div>
     );
   }
