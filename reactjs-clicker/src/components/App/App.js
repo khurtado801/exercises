76ae9bdb9d96f335bb0bff39a6d89901465fb320
ken.hurtado <khurtado@protonmail.com>
2018-04-23 14:03:41 -0600

    Simple clicker.

---
+++ reactjs-clicker/src/components/App/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Header from '../Header/Header';
+import Clicker from '../Clicker/Clicker';
+
+export default class ClickerApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'React-clicker'
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Header title={this.state.title} />
+        <div className="mt-5">
+          <Clicker />
+        </div>
+      </div>
+    );
+  }
+}
---
+++ reactjs-clicker/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/app.scss';
+import App from './components/App/App';
+
+ReactDOM.render(<App />, document.getElementById('app'));
