db64820e9c9cb3274beebbb6a1a64fb51efffa09
ken.hurtado <khurtado@protonmail.com>
2018-04-26 15:41:39 -0600

    Installed Yargs. Added feature to encode user input with encodeURIComponent for dynamic requests.

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,27 @@
 const request = require('request');
+const yargs = require ('yargs'); // Load in Yargs
+
+const argv = yargs.options ({
+    a: { // 'A' is for address
+        demand: true,
+        alias: 'address',
+        describe: 'Address to feetch weather for',
+        string: true
+    } // Object that stores final parsed object that was originally sent through yargs
+})
+.help()
+.alias('help', 'h') // Alias takes two arguments-first is the actual argument to set alias for, and second argument is the actual alias.
+.argv;
+
+let encodedAddress =  encodeURIComponent(argv.address) // Set equal to return value from encodeURIComponent 
+
 
 request({
- url: 'https://maps.googleapis.com/maps/api/geocode/json?address=2499%20Douglas%20st%20utah&key=AIzaSyD52Jc8l2IZc_86bilqqD66XkldvOVVlVo',
+ url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyA9XvMxvpE_AKCHONO6ISFH3Sca0yy9MOw`,
  json: true
 }, (error, response, body) => {
-    console.log(`Address:  ${body.results[0].formatted_address}`); // Template strings like 'Address: ${}' used for formatting
+    console.log(`Address:  ${body.results[0].formatted_address}`);
     console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
     console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
+
