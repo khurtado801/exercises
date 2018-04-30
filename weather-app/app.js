37be05556e9d562b0a2c0aff1ce9e35f8f5dfe6e
ken.hurtado <khurtado@protonmail.com>
2018-04-30 15:21:53 -0600

    Refactored app.js. Added Geocode.js which contains the request and error handling

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,5 @@
-const request = require('request');
 const yargs = require ('yargs'); // Load in Yargs
+const geocode = require('./Geocode/Geocode');
 
 const argv = yargs.options ({
     a: { // 'A' is for address
@@ -13,26 +13,10 @@ const argv = yargs.options ({
 .alias('help', 'h') // Alias takes two arguments-first is the actual argument to set alias for, and second argument is the actual alias.
 .argv;
 
-let encodedAddress =  encodeURIComponent(argv.address) // Set equal to return value from encodeURIComponent 
-
-
-request({
- url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyA9XvMxvpE_AKCHONO6ISFH3Sca0yy9MOw`,
- json: true
-}, (error, response, body) => {
-    /**
-     * Check status text value in results to determine whether or not a request was succesful.
-     * Check if status is set to OK to detemine that things went well.
-     * Between the status propery and the error object, we can determine what to do inside of the callback
-     */
-    if (error) { //check if error object exists and run code in block if error exists
-        console.log('Unable to connect to Google servers...');
-    } else if(body.status === 'ZERO_RESULTS') { // Check status property, if status property is ZERO_RESULTS
-        console.log('Unable to find that address...');
-    } else if(body.status === 'OK') {
-        console.log(`Address:  ${body.results[0].formatted_address}`);
-        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+    if (errorMessage) {
+        console.log(errorMessage);
+    } else {
+        console.log(JSON.stringify(results, undefined, 2));
     }
 });
-
