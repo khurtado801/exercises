a4da2efe21181da2a3ee4d609026f19a925ea274
ken.hurtado <khurtado@protonmail.com>
2018-04-30 14:50:44 -0600

    Added error handling for callback from Google API.

--- weather-app/app.js
+++ weather-app/app.js
@@ -20,8 +20,19 @@ request({
  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyA9XvMxvpE_AKCHONO6ISFH3Sca0yy9MOw`,
  json: true
 }, (error, response, body) => {
-    console.log(`Address:  ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    /**
+     * Check status text value in results to determine whether or not a request was succesful.
+     * Check if status is set to OK to detemine that things went well.
+     * Between the status propery and the error object, we can determine what to do inside of the callback
+     */
+    if (error) { //check if error object exists and run code in block if error exists
+        console.log('Unable to connect to Google servers...');
+    } else if(body.status === 'ZERO_RESULTS') { // Check status property, if status property is ZERO_RESULTS
+        console.log('Unable to find that address...');
+    } else if(body.status === 'OK') {
+        console.log(`Address:  ${body.results[0].formatted_address}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    }
 });
 
