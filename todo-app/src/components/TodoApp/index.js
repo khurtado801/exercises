9a55cd7ba08b9e1e3bbd1410bfdf2637b4e9901a
ken.hurtado <khurtado@protonmail.com>
2018-05-03 13:37:40 -0600

    Initial commit.

---
+++ todo-app/src/components/TodoApp/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TodoApp extends Component {
+    render () {
+        return (
+            <div>
+                TodoApp
+            </div>
+        );
+    }
+}
+
+export default TodoApp;
