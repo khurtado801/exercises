91a308512af1da711a5ca0999e299a893501f396
ken.hurtado <khurtado@protonmail.com>
2018-05-22 15:45:39 -0600

    Added a couple partials and some Handle Bar helpers.

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,34 +1,47 @@
 // Load in Express
 const express = require('express');
 // Load in our config
-const config = require("./config");
+const config = require('./config');
+const hbs = require('hbs');
 
 // Create new Express app
 // Save return result from calling Express as a function
 let app = express();
 
+// Let handlebars know that we want to add support for partials
+// Provide the directory you want to use for all of your handlebar partial files
+hbs.registerPartials(__dirname + '/views/partials');
+// Set various express configs
+app.set('view engine', 'hbs');
 // Middleware
 app.use(express.static(__dirname + '/public'));
+
+// Register Handle Bars helper.
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
 
 // Route handler for root
 app.get('/', (req, res) => {
 // Respond to request by sending data back, a string
     // res.send('<h1>Hello, Express!</h1>');
-    res.send({
-        name: 'Kenny',
-        likes: [
-            'Techno',
-            'House',
-            'Minimal',
-            'Dub'
-        ]
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website!',
     })
 });
 
 // Route handler for about
 app.get('/about', (req, res) => {
-// Respond to request by sending data back, a string
-    res.send('About Page')
+// Render is going to let us render any of the templates we have
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        // Create a new date
+    });
 })
 
 // Route handler for Bad
