09f29dcf94081cdd20e13210aebf2060090b6fe3
ken.hurtado <khurtado@protonmail.com>
2018-06-05 15:15:27 -0600

    Finsihed pushercoin PWA.

--- pushercoins/src/History/History.js
+++ pushercoins/src/History/History.js
@@ -10,24 +10,53 @@ class History extends Component {
             todayprice: {},
             yesterdayprice: {},
             twodaysprice: {},
-            threedayprice: {},
-            fourdayprice: {}
+            threedaysprice: {},
+            fourdaysprice: {}
         };
+    }
+
+    // This is called when an instance of a component is being created and inserted into the DOM
+    componentWillMount () {
+        this.getTodayPrice();
+        this.getYesterdayPrice();
+        this.getTwoDaysPrice();
+        this.getThreeDaysPrice();
+        this.getFourDaysPrice();
+    }
+
+    componentDidMount () {
+        if (!navigator.onLine) {
+            this.setState({
+                todayprice: JSON.parse(localStorage.getItem('todayprice')) 
+            });
+            this.setState({
+                yesterdayprice: JSON.parse(localStorage.getItem('yesterdayprice'))
+            });
+            this.setState({
+                twodaysprice: JSON.parse(localStorage.getItem('twodaysprice'))
+            });
+            this.setState({
+                threedaysprice: JSON.parse(localStorage.getItem('threedaysprice'))
+            });
+            this.setState({
+                fourdaysprice: JSON.parse(localStorage.getItem('fourdaysprice'))
+            });  
+        }
     }
 
     // This function gets the ETH price for a specific timestamp/date. The date is passed in as an argument
     getETHPrices = (date) => {
-        return axios.get('https://min-api.cryptocompare.com/data/pricehistorical?fsym=ETH&tsyms=USD&ts=' + date);
+        return axios.get(`https://min-api.cryptocompare.com/data/pricehistorical?fsym=ETH&tsyms=USD&ts=${date}`);
     };
 
     // This function gets the BTC price for a specific timestamp/date. The date is passed in as an argument
     getBTCPrices = (date) => {
-        return axios.get('https://min-api.cryptocompare.com/data/pricehistorical?fsym=BTC&tsyms=USD&ts=' + date);
+        return axios.get(`https://min-api.cryptocompare.com/data/pricehistorical?fsym=BTC&tsyms=USD&ts=${date}`);
     };
 
     // This function gets the LTC price for a specific timestamp/date. The date is passed in as an argument
     getLTCPrices = (date) => {
-        return axios.get('https://min-api.cryptocompare.com/data/pricehistorical?fsym=LTC&tsyms=USD&ts=' + date);
+        return axios.get(`https://min-api.cryptocompare.com/data/pricehistorical?fsym=LTC&tsyms=USD&ts=${date}`);
     }
     // This function gets the prices for the current date
     getTodayPrice = () => {
@@ -45,10 +74,14 @@ class History extends Component {
                 this.setState({
                     todayprice: f
                 });
+                localStorage.setItem('todayprice', JSON.stringify(f));
+                this.setState({
+                    todayprice: f
+                });
             }));
     }
 
-    // This function gets the price for the yesterday
+    // This function gets the price for yesterday
     getYesterdayPrice = () => {
         // Get yesterdays date in timestamp
         let t = moment().subtract(1, 'days').unix();
@@ -65,11 +98,161 @@ class History extends Component {
                 this.setState({
                     yesterdayprice: f
                 });
+                localStorage.setItem('yesterdayprice', JSON.stringify(f));
+                this.setState({
+                    yesterdayprice: f
+                });
             }));
     }
+
+    // This function gets the price for two days ago
+    getTwoDaysPrice = () => {
+        // Get the date for two days ago in timestamp
+        let t = moment().subtract(2, 'days').unix();
+        // Axios.all is used to make concurrent API requests. These requests were the functions we first created and they accept an argument of the date required.
+        axios.all([this.getETHPrices(t), this.getBTCPrices(t), this.getLTCPrices(t)])
+            .then(axios.spread((eth, btc, ltc) => {
+                let f = {
+                    date: moment.unix(t).format('MMMM Do YYYY'),
+                    eth: eth.data.ETH.USD,
+                    btc: btc.data.BTC.USD,
+                    ltc: ltc.data.LTC.USD
+                };
+                // Set the state of the twodayprice to the content of the object f
+                this.setState({
+                    twodaysprice: f
+                });
+                localStorage.setItem('twodaysprice', JSON.stringify(f));
+                this.setState({
+                    twodaysprice: f
+                });
+            }));
+    }
+
+    // This function gets the price for three days ago
+    getThreeDaysPrice = () => {
+        // Get the date for three days ago in timestamp
+        let t = moment().subtract(3, 'days').unix();
+        // Axios.all is used to make concurrent API requests. These requests were the functions we first created and they accept an argument of the date required
+        axios.all([this.getETHPrices(t), this.getBTCPrices(t), this.getLTCPrices(t)])
+            .then(axios.spread((eth, btc, ltc) => {
+                let f = {
+                    date: moment.unix(t).format('MMMM Do YYYY'),
+                    eth: eth.data.ETH.USD,
+                    btc: btc.data.BTC.USD,
+                    ltc: ltc.data.LTC.USD
+                };
+                // Set the state of the threedayprice to the content of the object f
+                this.setState({
+                    threedaysprice: f
+                });
+                localStorage.setItem('threedaysprice', JSON.stringify(f));
+                this.setState({
+                    threedaysprice: f
+                });
+            }));
+    }
+
+    // This function gets the price for four days ago
+    getFourDaysPrice = () => {
+        // Get the date for four days ago in timestamp
+        let t = moment().subtract(4, 'days').unix();
+        // Axios.all is used to make concurrent API requests. These requests were the functions we first created and they accept an argument of the date required
+        axios.all([this.getETHPrices(t), this.getBTCPrices(t), this.getLTCPrices(t)])
+            .then(axios.spread((eth, btc, ltc) => {
+                let f = {
+                    date: moment.unix(t).format('MMMM Do YYYY'),
+                    eth: eth.data.ETH.USD,
+                    btc: btc.data.BTC.USD,
+                    ltc: ltc.data.LTC.USD
+                };
+                // Set the state of fourdayprice to the content of the object f
+                this.setState({
+                    fourdaysprice: f
+                });
+                localStorage.setItem('fourdaysprice', JSON.stringify(f));
+                this.setState({
+                    fourdaysprice: f
+                });
+            }));
+    }
+
     render () {
         return (
-            <div>
+            <div className="history--section container">
+                    <h2>History (Past 5 days)</h2>
+                    <div className="history--section__box">
+                        <div className="history--section__box__inner">
+                            <h4>{this.state.todayprice.date}</h4>
+                            <div className="columns">
+                                <div className="column">
+                                    <p>1 BTC = ${this.state.todayprice.btc}</p>
+                                </div>
+                                <div className="column">
+                                    <p>1 ETH = ${this.state.todayprice.eth}</p>
+                                </div>
+                                <div className="column">
+                                    <p>1 LTC = ${this.state.todayprice.ltc}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="history--section__box__inner">
+                            <h4>{this.state.yesterdayprice.date}</h4>
+                            <div className="columns">
+                                <div className="column">
+                                    <p>1 BTC = ${this.state.yesterdayprice.btc}</p>
+                                </div>
+                                <div className="column">
+                                    <p>1 ETH = ${this.state.yesterdayprice.eth}</p>
+                                </div>
+                                <div className="column">
+                                    <p>1 LTC = ${this.state.yesterdayprice.ltc}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="history--section__box__inner">
+                            <h4>{this.state.twodaysprice.date}</h4>
+                            <div className="columns">
+                                <div className="column">
+                                    <p>1 BTC = ${this.state.twodaysprice.btc}</p>
+                                </div>
+                                <div className="column">
+                                    <p>1 ETH = ${this.state.twodaysprice.eth}</p>
+                                </div>
+                                <div className="column">
+                                    <p>1 LTC = ${this.state.twodaysprice.ltc}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="history--section__box__inner">
+                            <h4>{this.state.threedaysprice.date}</h4>
+                            <div className="columns">
+                                <div className="column">
+                                    <p>1 BTC = ${this.state.threedaysprice.btc}</p>
+                                </div>
+                                <div className="column">
+                                    <p>1 ETH = ${this.state.threedaysprice.eth}</p>
+                                </div>
+                                <div className="column">
+                                    <p>1 LTC = ${this.state.threedaysprice.ltc}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="history--section__box__inner">
+                            <h4>{this.state.fourdaysprice.date}</h4>
+                            <div className="columns">
+                                <div className="column">
+                                    <p>1 BTC = ${this.state.fourdaysprice.btc}</p>
+                                </div>
+                                <div className="column">
+                                    <p>1 ETH = ${this.state.fourdaysprice.eth}</p>
+                                </div>
+                                <div className="column">
+                                    <p>1 LTC = ${this.state.fourdaysprice.ltc}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
             </div>
         );
     }
--- pushercoins/src/Today/Today.js
+++ pushercoins/src/Today/Today.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Pusher from 'pusher-js';
 import './Today.css';
 
 class Today extends Component {
@@ -11,25 +12,82 @@ class Today extends Component {
             ethprice: ''
         };
     }
+
+
     componentWillMount() {
+        this.pusher = new Pusher('d60841d936650d3ff3dd', {
+            cluster: '4aec6072b3c5a89f99a9',
+            encrypted: true
+        });
+        // Subscribe to the 'coin-prices' channel
+        this.prices = this.pusher.subscribe('coin-prices');
+
         axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC&tsyms=USD')
             .then(response => {
                 // We set the latest prices in the state to the prices gotten from Cryptocurrency
                 this.setState({
                     btcprice: response.data.BTC.USD
                 });
+                localStorage.setItem('BTC', response.data.BTC.USD);
+
                 this.setState({
                     ethprice: response.data.ETH.USD
                 });
+                localStorage.setItem('ETH', response.data.ETH.USD);
+
                 this.setState({
                     ltcprice: response.data.LTC.USD
                 });
+                localStorage.setItem('LTC', response.data.LTC.USD);
             })
             // Catch any errors
             .catch(error => {
                 console.log(error);
             });
     }
+
+    componentDidMount () {
+        setInterval(() => {
+            if (!navigator.onLine) {
+                this.setState({
+                    btcprice: localStorage.getItem('BTC')
+                });
+                this.setState({
+                    ethprice: localStorage.getItem('ETH')
+                });
+                this.setState({
+                    ltcprice: localStorage.getItem('LTC')
+                });
+            }
+            axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC&tsyms=USD')
+                .then(response => {
+                    this.sendPricePusher(response.data);
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }, 10000);
+        this.prices.bind('prices', ({ prices: { BTC, ETH, LTC } }) => {
+            this.setState({
+                btcprice: BTC.usd,
+                ethprice: ETH.usd,
+                ltcprice: LTC.usd
+            }, this.saveStateToLocalStorage);
+        }, this);
+    }
+
+    sendPricePusher = (data) => {
+        axios.post('/prices/new', {
+            prices: data
+        })
+        .then(response => {
+            console.log(response);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    };
+
     render () {
         return (
             <div className="today--section container">
