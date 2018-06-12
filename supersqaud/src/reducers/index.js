ce44f3f4bc2d62777cdc2163aab115363b0a67f5
ken.hurtado <khurtado@protonmail.com>
2018-06-12 16:46:14 -0600

    Refactored.

--- supersqaud/src/reducers/index.js
+++ supersqaud/src/reducers/index.js
@@ -1,32 +1,7 @@
 import { combineReducers } from 'redux';
-import characters_json from '../data/characters.json';
-import { ADD_CHARACTER } from '../actions';
 
-function createCharacter (id) {
-    let character = characters_json.find(c => c.id === id);
-    return character;
-};
-
-function characters (state = characters_json, action) {
-    switch (action.type) {
-        case ADD_CHARACTER:
-            // Create new characters array
-            let characters = state.filter(item => item.id !== action.id);
-            return characters;
-        default:
-            return state;
-    }
-};
-
-function heroes (state = [], action) {
-    switch (action.type) {
-        case ADD_CHARACTER:
-            let heroes = [...state, createCharacter(action.id)];
-            return heroes;
-        default:
-            return state;
-    }
-};
+import characters from './characters_reducer';
+import heroes from './heroes_reducer';
 
 const rootReducer = combineReducers({
     characters,
