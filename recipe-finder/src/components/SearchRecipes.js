fa534f956def3a01546c0cd21e19a0c489e6aa03
ken.hurtado <khurtado@protonmail.com>
2018-06-20 07:51:05 -0600

    Added a form and event handlers to set local state to user input.

--- recipe-finder/src/components/SearchRecipes.js
+++ recipe-finder/src/components/SearchRecipes.js
@@ -2,22 +2,42 @@ import React, { Component } from 'react';
 import { Form, FormGroup, FormControl, ControlLabel, Button } from 'react-bootstrap';
 
 class SearchRecipes extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            ingredients: '',
+            dish: ''
+        };
+    }
+
+    search() {
+        const url = 'http://www.recipepuppy.com/api/';
+        console.log('state', this.state);
+    }
+
     render() {
         return (
             <Form inline>
                 <FormGroup>
                     <ControlLabel>Ingredients</ControlLabel>
                     {' '}
-                    <FormControl type="text" placeholder="garlic, chicken" />
+                    <FormControl type="text"
+                      placeholder="garlic, chicken"
+                      onChange={event => this.setState({ ingredients: event.target.value })}
+                    />
                 </FormGroup>
                 {' '}
                 <FormGroup>
                     <ControlLabel>Dish</ControlLabel>
                     {' '}
-                    <FormControl type="text" placeholder="adobo" />
+                    <FormControl type="text" 
+                      placeholder="adobo"
+                      onChange={event => this.setState({ dish: event.target.value })}
+                    />
                 </FormGroup>
                 {' '}
-                <Button>Submit</Button>
+                <Button onClick={() => this.search()}>Submit</Button>
             </Form>
         );
     }
