fe7da2d211848a6b549ba343965a0d4bd5304544
ken.hurtado <khurtado@protonmail.com>
2018-07-16 07:16:04 -0600

    Today our hero added MemeItem component to handle our memes. And basic styling was added to the MimeItem component.

--- meme-generator/src/components/App.js
+++ meme-generator/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import MemeItem from './MemeItem';
 import '../styles/index.css';
 
 class App extends Component {
@@ -18,7 +19,10 @@ class App extends Component {
                 {
                     this.props.memes.slice(0, this.state.memeLimit).map((meme, index) => {
                         return (
-                            <h4 key={index}>{meme.name}</h4>
+                            <MemeItem 
+                              key={index}
+                              meme={meme}
+                            />
                         );
                     })
                 }
