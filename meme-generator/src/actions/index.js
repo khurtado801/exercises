70991c3f78cf2b840aa859a6d787893d19f9d8c8
ken.hurtado <khurtado@protonmail.com>
2018-07-27 16:36:09 -0600

    Today our hero completed the logic for the POST meme action.

--- meme-generator/src/actions/index.js
+++ meme-generator/src/actions/index.js
@@ -1,3 +1,5 @@
+import { username, password } from './secrets';
+
 // Action type(s)
 export const RECEIVE_MEMES = 'RECEIVED_MEMES';
 export const NEW_MEME = 'NEW_MEME';
@@ -7,6 +9,7 @@ function receiveMemes(json) {
     const { memes } = json.data;
     // Action
     return {
+        // Action return
         type: RECEIVE_MEMES,
         memes
     };
@@ -26,9 +29,38 @@ export function fetchMemes() {
 }
 
 // Action creator
-export function newMeme(meme) {
+function newMeme(meme) {
+    // Action
     return {
+        // Action return
         type: NEW_MEME,
         meme
     };
 }
+
+// Action creator
+function postMemeJson(params) {
+    params["username"] = username;
+    params["password"] = password;
+  
+    const bodyParams = Object.keys(params).map(key => {
+      return encodeURIComponent(key) + '=' + encodeURIComponent(params[key]);
+    }).join('&');
+  
+    console.log('bodyParams', bodyParams);
+  
+    return fetch('https://api.imgflip.com/caption_image', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: bodyParams
+    }).then(response => response.json());
+  }
+  
+  export function createMeme(new_meme_object) {
+    return function(dispatch) {
+      return postMemeJson(new_meme_object)
+        .then(new_meme => dispatch(newMeme(new_meme)));
+    };
+  }
--- meme-generator/src/actions/secrets.js
+++ meme-generator/src/actions/secrets.js
@@ -1,4 +1,4 @@
 const username = 'khurtado801';
-const password = 'samadhi801!';
+const password = 'samadhi';
 
 export { username, password };
