f86ecfc3827fd882edcd094240423b759799c1e8
ken.hurtado <khurtado@protonmail.com>
2018-07-31 16:11:52 -0600

    Clean up.

--- NEW new-todo-app/app/app.jsx
+++
@@ -1,8 +0,0 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-
-ReactDOM.render(
-  // <Greeter name={firstName}/>,
-  <h1>Boilerplate app!</h1>,
-  document.getElementById('app')
-);
